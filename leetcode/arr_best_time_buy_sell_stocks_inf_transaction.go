a598ea5996dd045d7fba3ca5e68502f6490f9967
shubhamcs <nityo_shubham.b@nfcguest.com>
2025-01-25 14:08:45 +0530

    rename vars

--- leetcode/arr_best_time_buy_sell_stocks_inf_transaction.go
+++ leetcode/arr_best_time_buy_sell_stocks_inf_transaction.go
@@ -6,16 +6,16 @@ func MaxProfitInfTransaction(prices []int) int {
 	}
 
 	totalProfit := 0
-	buy_pos, sell_pos := 0, 0
+	buyPos, sellPos := 0, 0
 	for i := 1; i < len(prices); i++ {
 		if prices[i] >= prices[i-1] {
-			sell_pos++
+			sellPos++
 		} else {
-			totalProfit += prices[sell_pos] - prices[buy_pos]
-			buy_pos, sell_pos = i, i
+			totalProfit += prices[sellPos] - prices[buyPos]
+			buyPos, sellPos = i, i
 		}
 	}
-	totalProfit += prices[sell_pos] - prices[buy_pos]
+	totalProfit += prices[sellPos] - prices[buyPos]
 
 	return totalProfit
 }
