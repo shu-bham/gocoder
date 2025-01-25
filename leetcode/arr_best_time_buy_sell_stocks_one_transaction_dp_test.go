e142091ce51b4c924048dded41cc9fcdc3df8d5e
shubhamcs <nityo_shubham.b@nfcguest.com>
2025-01-25 12:23:12 +0530

    refactor tests

--- leetcode/arr_best_time_buy_sell_stocks_one_transaction_dp_test.go
+++ leetcode/arr_best_time_buy_sell_stocks_one_transaction_dp_test.go
@@ -1,32 +1,51 @@
 package leetcode_test
 
 import (
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	"gocoder/leetcode"
-	"testing"
 )
 
+// Test cases for MaxProfit function
+var maxProfitTestCases = []struct {
+	name     string
+	input    []int
+	expected int
+}{
+	{
+		name:     "Normal case with profit",
+		input:    []int{7, 1, 5, 3, 6, 4},
+		expected: 5,
+	},
+	{
+		name:     "Descending prices",
+		input:    []int{7, 6, 4, 3, 1},
+		expected: 0,
+	},
+	{
+		name:     "Single element array",
+		input:    []int{7},
+		expected: 0,
+	},
+	{
+		name:     "Two element array with no profit",
+		input:    []int{7, 6},
+		expected: 0,
+	},
+	{
+		name:     "Mixed prices with moderate profit",
+		input:    []int{3, 2, 6, 5, 0, 3},
+		expected: 3,
+	},
+}
+
+// TestMaxProfit runs all test cases for MaxProfit function
 func TestMaxProfit(t *testing.T) {
-	profit := leetcode.MaxProfit([]int{7, 1, 5, 3, 6, 4})
-	assert.Equal(t, 5, profit)
-}
-
-func TestMaxProfit2(t *testing.T) {
-	profit := leetcode.MaxProfit([]int{7, 6, 4, 3, 1})
-	assert.Equal(t, 0, profit)
-}
-
-func TestMaxProfit3(t *testing.T) {
-	profit := leetcode.MaxProfit([]int{7})
-	assert.Equal(t, 0, profit)
-}
-
-func TestMaxProfit4(t *testing.T) {
-	profit := leetcode.MaxProfit([]int{7, 6})
-	assert.Equal(t, 0, profit)
-}
-
-func TestMaxProfit5(t *testing.T) {
-	profit := leetcode.MaxProfit([]int{3, 2, 6, 5, 0, 3})
-	assert.Equal(t, 4, profit)
+	for _, tc := range maxProfitTestCases {
+		t.Run(tc.name, func(t *testing.T) {
+			profit := leetcode.MaxProfit(tc.input)
+			assert.Equal(t, tc.expected, profit, "Test case: %s", tc.name)
+		})
+	}
 }
