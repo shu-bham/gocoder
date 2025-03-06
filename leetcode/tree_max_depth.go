99e7cc0ddd8fbf2bc10b90129169733d8809f650
shubhamcs <nityo_shubham.b@nfcguest.com>
2025-03-06 22:26:54 +0530

    move out TreeNode

--- leetcode/tree_max_depth.go
+++ leetcode/tree_max_depth.go
@@ -3,11 +3,6 @@ package leetcode
 /**
  * Definition for a binary tree node.
  */
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
 
 func MaxDepth(root *TreeNode) int {
 	if root == nil {
