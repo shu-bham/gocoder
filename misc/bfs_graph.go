8853aa2ed932cd32a5eea918fbc4c9cbcd7ae531
shubhamcs <nityo_shubham.b@nfcguest.com>
2025-03-22 16:52:55 +0530

    Misc: Topological Sort

--- misc/bfs_graph.go
+++ misc/bfs_graph.go
@@ -2,23 +2,6 @@ package misc
 
 import "fmt"
 
-type Graph struct {
-	adjList map[int][]int
-}
-
-func NewGraph() *Graph {
-	return &Graph{adjList: make(map[int][]int)}
-}
-
-func (g *Graph) AddEdge(u, v int) {
-	g.adjList[u] = append(g.adjList[u], v)
-	g.adjList[v] = append(g.adjList[v], u)
-}
-
-func (g *Graph) GetAdjList() map[int][]int {
-	return g.adjList
-}
-
 func BFS(edges [][]int, src int) []int {
 	g := makeGraph(edges)
 	visited := make(map[int]bool)
