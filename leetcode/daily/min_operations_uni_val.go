69995ea5817e05a9c00f7ec7d677150222456f74
shubhamcs <nityo_shubham.b@nfcguest.com>
2025-03-26 21:29:43 +0530

    Min Operations to make uni values grid : Medium

---
+++ leetcode/daily/min_operations_uni_val.go
@@ -0,0 +1,45 @@
+package leetcode
+
+import (
+	"math"
+	"sort"
+)
+
+func MinOperationsToMakeUniVal(grid [][]int, x int) int {
+	n, m := len(grid), len(grid[0])
+	rem := grid[0][0] % x
+
+	temp := make([]int, 0)
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if grid[i][j]%x != rem {
+				return -1
+			}
+			temp = append(temp, grid[i][j])
+		}
+	}
+
+	sort.Ints(temp)
+
+	// median is the optimal
+	target := temp[len(temp)/2]
+
+	targets := []int{target}
+
+	minAns := math.MaxInt
+
+	for _, t := range targets {
+		ans := 0
+		for i := 0; i < n; i++ {
+			for j := 0; j < m; j++ {
+				f := float64(t - grid[i][j])
+				ans += int(math.Abs(f)) / x
+			}
+		}
+		minAns = min(minAns, ans)
+	}
+
+	return minAns
+
+}
