da94cfa067d9cf8b76b65c25e85094bd1ffc25af
shu-bham <shoobham25@gmail.com>
2026-01-24 22:52:07 +0530

    more changes

--- google/54.int_to_roman.go
+++ google/54.int_to_roman.go
@@ -9,19 +9,19 @@ func intToRoman(num int) string {
 
 	symbols := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
 	values := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
-	var ans strings.Builder
 
-	i := 0
-	for num > 0 {
-		for i < len(values) {
-			if num >= values[i] {
-				ans.WriteString(symbols[i])
-				num -= values[i]
-			} else {
-				break
-			}
+	var roman strings.Builder
+
+	// Iterate through each value/symbol pair.
+	for i := 0; i < len(values); i++ {
+		// Greedily subtract the current value from num as many times
+		// as possible, appending the symbol each time.
+		for num >= values[i] {
+			roman.WriteString(symbols[i])
+			num -= values[i]
 		}
-		i++
 	}
-	return ans.String()
+
+	return roman.String()
+
 }
